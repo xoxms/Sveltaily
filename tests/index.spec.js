590d54d03b383775a02d4b9514a6fd3b6e724d7a
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-04-03 21:36:20 +0700

    Change test description

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test('page title should be shown', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await expect(await page.locator(".page-title")).toContainText("Welcome to")
 });
