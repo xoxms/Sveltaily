bfb33727af77113a313d364040229d2e348d0187
Tinnaphat "Tin" Somsang <tinvv@outlook.co.th>
2022-05-18 22:01:17 +0700

    fix: head tags not showing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,11 +2,13 @@
   import { darkTheme } from '$lib/store';
   import Kofi from '$lib/components/Kofi.svelte';
   import SocialButtons from '$lib/components/SocialButtons.svelte';
+  import Head from '$lib/components/Head.svelte';
   import '../tailwind.css';
   import '../index.css';
 </script>
 
 <div class={$darkTheme ? 'dark' : ''}>
+  <Head />
   <button class="absolute left-2 top-2 text-lg" on:click={() => ($darkTheme = !$darkTheme)}>
     {$darkTheme ? '🌙' : '🌞'}
   </button>
